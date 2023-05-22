7c4e0869cf1f9a10537bc045e31f9525cd8a6df2
Matthew Scarth-Saunders <101186068+MattScarthSaunders@users.noreply.github.com>
2023-05-22 13:55:04 +0100

    fix: json string output to blob

--- BlobWriter/__init__.py
+++ BlobWriter/__init__.py
@@ -6,5 +6,5 @@ import json
 def main(inBlob: func.InputStream, outBlob: func.Out[bytes]):
     logging.info(f'Function successfully triggered by blob: {inBlob.name}')
     jsonData= json.loads(inBlob.read())
-    outBlob.set(jsonData)
+    outBlob.set(json.dumps(jsonData))
     
