1d680199e4fe638b7abdadf3f97f9d0b874adae2
Matthew Scarth-Saunders <101186068+MattScarthSaunders@users.noreply.github.com>
2023-05-22 14:39:02 +0100

    test: timed a to blob, a to ab in other blob

---
+++ function_a/__init__.py
@@ -0,0 +1,17 @@
+import datetime
+import logging
+import json
+import azure.functions as func
+
+
+def main(mytimer: func.TimerRequest, outBlob: func.Out[bytes]) -> None:
+    logging.info('Timer Triggered a.json creation')
+
+    a_json = {
+        "name": 'test_func',
+        "value": 'a'
+    }
+
+    mod_data = json.dumps(a_json)
+    
+    outBlob.set(mod_data.encode('utf-8'), name='a')
