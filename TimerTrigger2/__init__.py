ad52c90d85a209dceb10c3ad1bbd836ee2e83fe2
Matthew Scarth-Saunders <101186068+MattScarthSaunders@users.noreply.github.com>
2023-05-22 11:17:16 +0100

    fix: timertrigger2 param

--- TimerTrigger2/__init__.py
+++ TimerTrigger2/__init__.py
@@ -4,11 +4,11 @@ import logging
 import azure.functions as func
 
 
-def main(mytimer: func.TimerRequest) -> None:
+def main(mytimer2: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
 
-    if mytimer.past_due:
+    if mytimer2.past_due:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
