aa246a1e11855926f3975ecbc10bda75e4b1cc48
Teyah Brennen-Davies <teyahbrennendavies@gmail.com>
2023-05-30 12:27:36 +0100

    refactor: add bytesio

--- processor_course_report/__init__.py
+++ processor_course_report/__init__.py
@@ -1,11 +1,12 @@
 import azure.functions as func
 import pandas as pd
 from .app import app
+from io import BytesIO
 
 
-def main(inBlob: func.InputStream, outBlob: func.Out[str]):
+def main(inBlob: func.InputStream, outBlob: func.Out[bytes]):
 
-    input_df = pd.read_json(inBlob.read())
+    input_df = pd.read_json(BytesIO(inBlob.read()))
 
     output_df = app(input_df)
 
