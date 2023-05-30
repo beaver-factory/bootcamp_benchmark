3f4ba3e972e6e47ce84bc841a6eed6891392c69c
Matthew Scarth-Saunders <101186068+MattScarthSaunders@users.noreply.github.com>
2023-05-30 11:34:07 +0100

    chore: removing test dir

--- test_dir/collector_course_report_nest/app.py
+++
@@ -1,9 +0,0 @@
-from .spider import CourseReportSpider
-from scrapyscript import Job, Processor
-
-
-def get_scraped_data():
-    processor = Processor()
-    job = Job(CourseReportSpider)
-    results = processor.run(job)
-    return results
