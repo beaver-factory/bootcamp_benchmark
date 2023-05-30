f5ab67aa1ed5c51d89d9f36940cc0ac0f5e16ecc
xavbruggen <xavier.bruggen@northcoders.com>
2023-05-30 14:50:28 +0100

    ci: function conformity

---
+++ script/func_check.py
@@ -0,0 +1,32 @@
+import os
+
+excluded_directories = ['script', 'env']
+
+valid_directory_prefixes = ['collector_', 'processor_', 'loader_']
+
+def check_directory_naming():
+    current_directory = os.getcwd()
+
+    directories = []
+
+    for item in os.listdir(current_directory):
+        if os.path.isdir(item) and not item.startswith('.') and item not in excluded_directories:
+            directories.append(item)
+
+    invalid_directories = []
+
+    for directory in directories:
+        if not directory.startswith(tuple(valid_directory_prefixes)):
+            invalid_directories.append(directory)
+
+    if len(invalid_directories) > 0:
+        print('\nInvalid directories found:\n')
+        for directory in invalid_directories:
+            print(directory)
+        print('\nPlease rename them to start with one of the following prefixes:\n')
+        for prefix in valid_directory_prefixes:
+            print(prefix)
+
+        exit(1)
+
+check_directory_naming()
