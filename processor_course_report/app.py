9bb902b0d9a2103c59464d72ec2d714c8af88973
Chris-Duignan <cduignan.1993@gmail.com>
2023-05-30 15:01:37 +0100

    feat: create time column for part or full time

--- processor_course_report/app.py
+++ processor_course_report/app.py
@@ -1,14 +1,24 @@
 import pandas as pd
 
 
-def app(course_dataframe):
+def find_time_commitment(x):
+    if 'part time' in x.course_name.lower():
+        return 'part_time'
+    if 'full time' in x.course_name.lower():
+        return 'full_time'
+    else:
+        return None
 
+
+def app(course_dataframe):
     df = course_dataframe.explode('provider_courses').reset_index()
 
     normalised_df = pd.json_normalize(df.provider_courses)
 
     concat_normalised_df = pd.concat([df, normalised_df], axis=1).drop(
         ['provider_courses', 'provider_locations'], axis=1)
+
+    concat_normalised_df['time'] = concat_normalised_df.apply(lambda x: find_time_commitment(x), axis=1)
 
     exploded_locations = concat_normalised_df.explode(
         'course_skills').reset_index().drop(['index', 'level_0'], axis=1)
