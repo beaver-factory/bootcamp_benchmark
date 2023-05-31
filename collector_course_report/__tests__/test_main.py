db190e0a3cafb2586949aae356df366e0eaaf17f
xavbruggen <xavier.bruggen@northcoders.com>
2023-05-31 10:56:58 +0100

    test: asserting on invoking azure correctly

---
+++ collector_course_report/__tests__/test_main.py
@@ -0,0 +1,28 @@
+import unittest
+from unittest.mock import MagicMock, patch
+import importlib
+import azure.functions as func
+import collector_course_report
+
+
+class TestMain(unittest.TestCase):
+
+    @patch('collector_course_report.app.get_scraped_data', return_value={'foo': 'bar'})
+    def test_main(self, mock_get_scraped_data):
+        # annoyingly have to reload module to ensure that the foobar mock is applied
+        importlib.reload(collector_course_report)
+
+        # arrange
+        mytimer = MagicMock()
+        outBlob = MagicMock(spec=func.Out[bytes])
+
+        # act
+        collector_course_report.main(mytimer, outBlob)
+
+        # assert
+        mock_get_scraped_data.assert_called_once()
+        outBlob.set.assert_called_once_with(b'{"foo": "bar"}')
+
+
+if __name__ == '__main__':
+    unittest.main()
