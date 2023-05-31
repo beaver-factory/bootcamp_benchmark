8890460c48c3effc80172f7727dda92e5bd6d56c
Chris-Duignan <cduignan.1993@gmail.com>
2023-05-31 12:27:14 +0100

    test: test dataframe contains correct columns

--- processor_course_report/__tests__/test_app.py
+++ processor_course_report/__tests__/test_app.py
@@ -2,6 +2,7 @@ from ..app import process_scraped_data
 import pytest
 import pandas as pd
 import json
+import copy
 
 
 @pytest.fixture(scope="session", autouse=False)
@@ -16,7 +17,7 @@ def expected_data_structure():
                     "course_name": "",
                     "course_skills": [""],
                     "course_locations": "",
-                    "course_desc": ""
+                    "course_description": ""
                 }
             ],
             "meta": {
@@ -33,7 +34,7 @@ def expected_data_structure():
                     "course_name": "",
                     "course_skills": [""],
                     "course_locations": "",
-                    "course_desc": ""
+                    "course_description": ""
                 }
             ],
             "meta": {
@@ -53,7 +54,7 @@ def test_raises_exception_on_incorrect_shape_at_first_level():
 
 
 def test_raises_exception_on_incorrect_shape_at_nest(expected_data_structure):
-    error_structure = expected_data_structure
+    error_structure = copy.deepcopy(expected_data_structure)
     del error_structure[0]['provider_courses'][0]['course_skills']
     del error_structure[1]['provider_courses'][0]['course_skills']
     df = pd.read_json(json.dumps(error_structure))
@@ -63,15 +64,22 @@ def test_raises_exception_on_incorrect_shape_at_nest(expected_data_structure):
 
     assert 'course_skills' in str(excinfo.value)
 
+def test_returns_pandas_dataframe(expected_data_structure):
+    result = process_scraped_data(pd.read_json(json.dumps(expected_data_structure)))
+    assert isinstance(result, pd.DataFrame)
 
-def test_raises_exception_on_incorrect_type(expected_data_structure):
-    error_structure = expected_data_structure
-    error_structure[0]['provider_courses'] = "hi"
-    df = pd.read_json(json.dumps(error_structure))
+def test_dataframe_contains_correct_columns(expected_data_structure):
+    result = process_scraped_data(pd.read_json(json.dumps(expected_data_structure)))
+    expected = [
+        'provider_name',
+        'provider_tracks',
+        'course_name',
+        'course_skills',
+        'course_locations',
+        'course_description',
+        'time',
+        'target_url',
+        'timestamp'
+    ]
 
-    with pytest.raises(Exception) as excinfo:
-        process_scraped_data(df)
-
-    print(excinfo.traceback)
-
-    assert 'Field is wrong datatype' in str(excinfo.value)
+    assert all(column in result.columns.values for column in expected)
