bf91dc064eff2de76e6a57b4cf3c7e5cde30106a
xavbruggen <xavier.bruggen@northcoders.com>
2023-05-31 13:13:41 +0100

    test: manual mock

--- collector_course_report/__tests__/test_main.py
+++ collector_course_report/__tests__/test_main.py
@@ -2,17 +2,23 @@ import json
 import azure.functions as func
 from collector_course_report import main
 
+class OutBlobMock:
+    def __init__(self):
+        self.data = None
+
+    def set(self, data):
+        self.data = data
 
 def test_main(mocker):
     # arrange
     mock_get_scraped_data = mocker.patch('collector_course_report.get_scraped_data')
     mock_get_scraped_data.return_value = {"foo": "bar"}
 
-    mock_out_blob = mocker.Mock(spec=func.Out[bytes])
+    mock_out_blob = OutBlobMock()
 
     # act
     main(None, mock_out_blob)
 
     # assert
     mock_get_scraped_data.assert_called_once()
-    mock_out_blob.set.assert_called_once_with(json.dumps({"foo": "bar"}).encode('utf-8'))
+    assert mock_out_blob.data == json.dumps({"foo": "bar"}).encode('utf-8')
