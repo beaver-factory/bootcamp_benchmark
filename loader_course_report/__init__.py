718907e3a2054c4d76510be0667274146be37dae
alexcupit <alexcupit@outlook.com>
2023-06-01 11:41:39 +0100

    chore: remove comments

--- loader_course_report/__init__.py
+++ loader_course_report/__init__.py
@@ -1,11 +1,6 @@
 import azure.functions as func
-# import pandas as pd
 from .app import load_course_report_into_db
 
 
 def main(inBlob: func.InputStream):
-    # read data from blob and store as data frame
-    # input_df = pd.read_csv(inBlob.read())
-
-    # load data frame into db
     load_course_report_into_db(inBlob)
