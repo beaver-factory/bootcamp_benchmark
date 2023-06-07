e63031bcaf6f6c9d0051246e57042cc390f103a9
Matthew Scarth-Saunders <101186068+MattScarthSaunders@users.noreply.github.com>
2023-06-07 15:03:23 +0100

    feat: skills loader function (prev is tests)

--- loader_course_report/app.py
+++ loader_course_report/app.py
@@ -11,24 +11,18 @@ def load_course_report_into_db(inBlob: func.InputStream):
 
     column_headers = ['provider_name', 'course_name', 'course_skills', 'course_locations', 'course_description', 'target_url', 'timestamp', 'course_country']
 
-    if df.columns.values.tolist() != column_headers:
-        raise Exception('Invalid CSV column names')
-
-    if len(df.index) == 0:
-        raise Exception('CSV only has headers')
-
-    if all(df.iloc[0].isnull().values.tolist()):
-        raise Exception('CSV has headers but no data')
+    handle_errors(column_headers, df)
 
     # establish connection to db, using env variable,
     # either ARM template connectionstring for production or .env for testing'
     conn = psycopg2.connect(os.environ["PSQL_CONNECTIONSTRING"])
     cur = conn.cursor()
+    table_name = 'course_report'
 
-    cur.execute('DROP TABLE IF EXISTS course_report')
+    cur.execute(f'DROP TABLE IF EXISTS {table_name}')
 
-    cur.execute('''
-            CREATE TABLE course_report (
+    cur.execute(f'''
+            CREATE TABLE {table_name} (
                 ID SERIAL PRIMARY KEY,
                 provider_name VARCHAR(50),
                 course_name VARCHAR(100),
@@ -48,7 +42,7 @@ def load_course_report_into_db(inBlob: func.InputStream):
     args_str = ','.join(cur.mogrify(
         "(%s,%s,%s,%s,%s,%s,%s,%s)", x).decode('utf-8') for x in tup)
 
-    cur.execute("""INSERT INTO course_report (
+    cur.execute(f"""INSERT INTO {table_name} (
                  provider_name,
                  course_name,
                  skill,
@@ -65,3 +59,53 @@ def load_course_report_into_db(inBlob: func.InputStream):
     # close it all down
     cur.close()
     conn.close()
+
+
+def load_course_skills_into_db(inBlob: func.InputStream):
+    df = pd.read_csv(BytesIO(inBlob.read()))
+    df.pop(df.columns[0])
+
+    column_headers = ['course_skills']
+
+    handle_errors(column_headers, df)
+
+    # establish connection to db, using env variable,
+    # either ARM template connectionstring for production or .env for testing'
+    conn = psycopg2.connect(os.environ["PSQL_CONNECTIONSTRING"])
+    cur = conn.cursor()
+    table_name = 'course_skills'
+
+    cur.execute(f'DROP TABLE IF EXISTS {table_name}')
+
+    cur.execute(f'''
+            CREATE TABLE {table_name} (
+                ID SERIAL PRIMARY KEY,
+                skill VARCHAR(50)
+            );
+            ''')
+
+    tup = list(df.itertuples(index=False))
+
+    # converting df rows into string for SQL query, while protecting from injection
+    # enables multiple rows to be added to query string
+    args_str = ','.join(cur.mogrify("(%s)", x).decode('utf-8') for x in tup)
+
+    cur.execute(f"""INSERT INTO {table_name} (skill) VALUES """ + args_str)
+
+    # !Important, make changes persist on db!
+    conn.commit()
+
+    # close it all down
+    cur.close()
+    conn.close()
+
+
+def handle_errors(column_headers, df):
+    if df.columns.values.tolist() != column_headers:
+        raise Exception('Invalid CSV column names')
+
+    if len(df.index) == 0:
+        raise Exception('CSV only has headers')
+
+    if all(df.iloc[0].isnull().values.tolist()):
+        raise Exception('CSV has headers but no data')
