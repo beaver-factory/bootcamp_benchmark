41cbd970c073f37b839e63c42b0021b6c8feb7d7
Matthew Scarth-Saunders <101186068+MattScarthSaunders@users.noreply.github.com>
2023-06-07 15:56:54 +0100

    fix: blob name

--- loader_course_report/__init__.py
+++ loader_course_report/__init__.py
@@ -2,9 +2,9 @@ import azure.functions as func
 from .app import load_course_report_into_db, load_course_skills_into_db
 
 
-def main(inBlob: func.InputStream, blobName: str):
-    if 'processed_course_data_' in blobName:
+def main(inBlob: func.InputStream):
+    if 'processed_course_data_' in inBlob.name:
         load_course_report_into_db(inBlob)
 
-    if 'processed_skills_data_' in blobName:
+    if 'processed_skills_data_' in inBlob.name:
         load_course_skills_into_db(inBlob)
