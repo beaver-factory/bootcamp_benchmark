a204d3071ca34c0a6a0005381362b9bfaf9ef8af
Teyah Brennen-Davies <teyahbrennendavies@gmail.com>
2023-06-07 12:58:21 +0100

    feat: add input blob for skills data

--- collector_adzuna/__init__.py
+++ collector_adzuna/__init__.py
@@ -1,11 +1,8 @@
 from .app import collector_adzuna
 import azure.functions as func
-import json
 
 
-def main(mytimer: func.TimerRequest, outBlob: func.Out[bytes]) -> None:
+def main(mytimer: func.TimerRequest, inBlob: func.InputStream, outBlob: func.Out[bytes]) -> None:
+    api_data = collector_adzuna(inBlob)
 
-    api_data = collector_adzuna()
-
-    json_data = json.dumps(api_data)
-    outBlob.set(json_data.encode('utf-8'))
+    outBlob.set(api_data.encode('utf-8'))
