bd9e65c0e8e6b796f58175c1327766102f7cf042
Chris-Duignan <cduignan.1993@gmail.com>
2023-06-12 16:03:47 +0100

    refactor: remove try except and implement if

--- processor_course_report/__init__.py
+++ processor_course_report/__init__.py
@@ -7,9 +7,9 @@ import logging
 
 def main(inBlob: func.InputStream, outBlob: func.Out[bytes], outBlob2: func.Out[bytes], outBlob3: func.Out[bytes]):
 
-    try:
-        input_json = inBlob.read().decode('utf-8')
-    except json.JSONDecodeError:
+    input_json = inBlob.read().decode('utf-8')
+
+    if input_json == '':
         raise Exception('Unprocessed json is empty, check json output from collector')
 
     input_obj = json.loads(input_json)
@@ -29,3 +29,4 @@ def main(inBlob: func.InputStream, outBlob: func.Out[bytes], outBlob2: func.Out[
     skills_csv = skills_df.to_csv()
     outBlob2.set(skills_csv.encode('utf-8'))
     outBlob3.set(skills_csv.encode('utf-8'))
+    
