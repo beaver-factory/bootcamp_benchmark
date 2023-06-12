de655dc228a8fef982407c2261c38aaf8387c2d8
Alex Cupit <alexcupit@outlook.com>
2023-06-12 14:34:05 +0100

    docs: add pydocs and logging to loader_adzuna

--- loader_adzuna/app.py
+++ loader_adzuna/app.py
@@ -3,9 +3,14 @@ import azure.functions as func
 import psycopg2
 import os
 from io import BytesIO
+import logging
 
 
 def load_adzuna_jobs_per_skill(inBlob: func.InputStream):
+    """
+    Creates a connection to the PSQL server before creating adzuna_job_counts table and inserting data.
+
+    """
     df = pd.read_csv(BytesIO(inBlob.read()))
 
     column_headers = ['skill', 'number_of_jobs']
@@ -15,6 +20,7 @@ def load_adzuna_jobs_per_skill(inBlob: func.InputStream):
     # establish connection to db, using env variable,
     # either ARM template connectionstring for production or .env for testing'
     conn = psycopg2.connect(os.environ["PSQL_CONNECTIONSTRING"])
+    logging.info('Successfully connected to PSQL server using PSQL_CONNECTIONSTRING to load Adzuna jobs data')
     cur = conn.cursor()
     table_name = 'adzuna_job_counts'
 
@@ -28,6 +34,8 @@ def load_adzuna_jobs_per_skill(inBlob: func.InputStream):
             );
             ''')
 
+    logging.info('Successfully created adzuna_job_counts table')
+
     tup = list(df.itertuples(index=False))
 
     # converting df rows into string for SQL query, while protecting from injection
@@ -39,6 +47,8 @@ def load_adzuna_jobs_per_skill(inBlob: func.InputStream):
                  skill,
                  job_count
              ) VALUES """ + args_str)
+
+    logging.info('Successfully inserted values into adzuna_job_counts table')
 
     # !Important, make changes persist on db!
     conn.commit()
