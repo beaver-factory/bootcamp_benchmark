c124ebf3d8da30b58f25b043e4fc58b27a27a600
Chris-Duignan <cduignan.1993@gmail.com>
2023-06-14 12:30:08 +0100

    refactor: remove old functions from root

--- processor_adzuna/__init__.py
+++
@@ -1,9 +0,0 @@
-from .app import process_adzuna_data
-
-import azure.functions as func
-
-
-def main(inBlob: func.InputStream, outBlob: func.Out[bytes]) -> None:
-    csv_file = process_adzuna_data(inBlob)
-
-    outBlob.set(csv_file.getvalue().encode('utf-8'))
