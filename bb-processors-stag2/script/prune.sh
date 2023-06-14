189e72f802533935caecc959c2f4129bcb619c0b
Chris-Duignan <cduignan.1993@gmail.com>
2023-06-14 14:32:58 +0100

    refactor: keep copy of scripts in processors

---
+++ bb-processors-stag2/script/prune.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# build new reqs from current project
+pipreqs ./ --ignore venv --savepath requirements2.txt
+
+# dev dependencies
+declare -a dev_dependencies=("pipreqs==0.4.13" "flake8==6.0.0" "pytest==7.3.1", "azure-functions==1.14.0")
+
+# diff the two requirements files
+diff --changed-group-format='%<' --unchanged-group-format='' requirements.txt requirements2.txt > temp_requirements.txt
+
+# filter out the dev dependencies
+for dep in "${dev_dependencies[@]}"
+do
+  grep -v "$dep" temp_requirements.txt > surplus_requirements.txt
+  cp surplus_requirements.txt temp_requirements.txt
+done
+
+# count total surplus dependencies
+surplus_count=$(wc -l < surplus_requirements.txt)
+echo "Total number of surplus dependencies in the project: $surplus_count"
+
+echo "Surplus dependencies:"
+cat surplus_requirements.txt
+
+# cleanup
+rm requirements2.txt
+rm temp_requirements.txt
+rm surplus_requirements.txt
+
+if [ $surplus_count -gt 0 ]
+then
+  echo "Error: Surplus dependencies found. If any of these are dev dependencies, add them to the exclusions list in the dev_dependencies array of this script." >&2
+  exit 1
+fi
---
+++ bb-processors-stag2/script/tag.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+echo -e "\n"
+
+tag_version=$1
+
+# Check if the user provided a valid tag version
+if [ "$tag_version" != "major" ] && [ "$tag_version" != "minor" ] && [ "$tag_version" != "patch" ]; then
+    echo "❌ Invalid tag version. Please provide either 'major', 'minor', or 'patch'."
+    exit 0
+fi
+
+current_branch=$(git symbolic-ref --short HEAD)
+
+# Check if the current branch is not 'main'
+if [ "$current_branch" != "main" ]; then
+    echo "❌ Current branch is not 'main'. You're on '$current_branch' branch."
+    exit 1
+fi
+
+echo -e "✅ Current branch is  'main'.\n"
+
+# # Check if main is up-to-date with remote
+
+echo -e "• Checking if 'main' is up-to-date with remote:\n"
+
+git fetch origin main
+
+if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
+    echo "❌ 'main' is not up-to-date with remote. Please pull the latest changes."
+    exit 1
+fi
+
+# Delete all local Git tags
+git tag -l | xargs git tag -d
+# Fetching all tags from remote repository
+echo "Fetching all tags from remote repository..."
+git fetch --tags
+
+# Get the most recent tag
+recent_tag=$(git describe --tags `git rev-list --tags --max-count=1` 2>/dev/null)
+
+new_tag=""
+
+if [ -z "$recent_tag" ]; then
+    # If no tags found, create the first ever tag
+    echo -e "👀 No tags found in the repository.\n"
+    new_tag="v0.1.0"
+    echo -e "✅ Creating first tag: $new_tag\n"
+elif [ "$tag_version" == "patch" ]; then
+    echo -e "• Applying PATCH update:\n"
+
+    version_parts=(${recent_tag//./ })
+    ((version_parts[2]++))
+    new_tag="${version_parts[0]}.${version_parts[1]}.${version_parts[2]}"
+
+    echo -e "\t $recent_tag → $new_tag \n"
+elif [ "$tag_version" == "minor" ]; then
+    echo -e "• Applying MINOR update:\n"
+
+    version_parts=(${recent_tag//./ })
+    ((version_parts[1]++))
+    version_parts[2]=0
+    new_tag="${version_parts[0]}.${version_parts[1]}.${version_parts[2]}"
+
+    echo -e "\t $recent_tag → $new_tag \n"
+elif [ "$tag_version" == "major" ]; then
+    echo -e "• Applying MAJOR update:\n"
+
+    version_parts=(${recent_tag//v/ }) # Remove 'v' from version
+    version_numbers=(${version_parts[0]//./ }) # Split version numbers
+
+    ((version_numbers[0]++))
+    version_numbers[1]=0
+    version_numbers[2]=0
+    new_tag="v${version_numbers[0]}.${version_numbers[1]}.${version_numbers[2]}"
+
+    echo -e "\t $recent_tag → $new_tag \n"
+else
+    echo "❌ Tag version is not 'patch', 'minor' or 'major'. No update applied."
+    exit 0
+fi
+
+echo -e "❓ Type 'YEE-HAW' to publish new tag:\n"
+read user_input
+
+# Check the user's input
+if [ "$user_input" == "YEE-HAW" ]; then
+    echo -e "\n"
+    # publish new tag
+    git tag -a $new_tag -m "auto-generated tag"
+    git push origin $new_tag
+    echo "✅ published new tag: $new_tag"
+else
+    echo "❌ Exiting..."
+    exit 1
+fi
+
+
+echo -e "\n"
