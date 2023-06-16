13b9a6cd2b59b8f9f55878a193a636f2a05fdd09
Chris-Duignan <cduignan.1993@gmail.com>
2023-06-16 11:17:37 +0100

    refactor: update make func script path

--- script/funcs.sh
+++ script/funcs.sh
@@ -2,6 +2,6 @@
 
 directory=$1
 
-cp an_example_function/app.py $directory
-cp -r an_example_function/__tests__ $directory/__tests__
+cp ../an_example_function/app.py $directory
+cp -r ../an_example_function/__tests__ $directory/__tests__
 rm $directory/readme.md
