f044b94c9951518cb3a3fc3787fa663f78f26d97
Chris-Duignan <cduignan.1993@gmail.com>
2023-06-21 16:56:28 +0100

    feat: refined prompt

--- processors/processor_course_report/extract_skills.py
+++ processors/processor_course_report/extract_skills.py
@@ -9,20 +9,20 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def extract_skills(description):
 
-    technologies = 'JavaScript, Front End'
+    technologies = 'JavaScript, Front-End'
 
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=f'Extract a list of programming skills, technologies and frameworks from the following description:\n\n """{description}""" \n which are similar to or equal to these example technologies: \n """{technologies}""" ',
-        temperature=1,
+        prompt=f'Extract a single, comma separated list of technologies, programming languages, and frameworks from the following description where the course purports to teach this :\n\n """{description}""" \n which are similar to or equal to these examples: \n """{technologies}""" ',
         max_tokens=100,
         top_p=0.1,
+        temperature=1,
         frequency_penalty=0.8,
         presence_penalty=0.0
     )
 
-    output = response.choices[0].text
+    output = response.choices[0].text.strip()
 
     print('OUTPUT', output)
 
-    return output.split('\n-')
+    return output.split(', ')
