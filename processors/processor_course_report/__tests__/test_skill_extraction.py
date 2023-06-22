a771469ce4f485435363f5445358ee5cb88557f6
Alex Cupit <alexcupit@outlook.com>
2023-06-22 11:47:21 +0100

    feat: updated prompt and adding into notebook

--- processors/processor_course_report/__tests__/test_skill_extraction.py
+++ processors/processor_course_report/__tests__/test_skill_extraction.py
@@ -1,17 +1,26 @@
 from ..extract_skills import extract_skills
 
+test_input = "As a Software Engineer graduate you will be ready to start a career in a variety of coding roles. Throughout this bootcamp you will learn to: create front-end web application with modern JavaScript frameworks such as Angular or React, develop full-stack applications with in-demand technologies such as Ruby on Rails, Python with Django, and Express with Node.js, and integrate third-party application programming interfaces (APIs) in an application."
+
+test_input2 = "Become a software engineer in 13 weeks at our coding bootcamps in Manchester, Leeds, Newcastle, Birmingham and remotely. Unlike our Data Engineering bootcamp where you focus specifically on the 'back-end' of software, or our DevOps Engineering bootcamp that deals specifically with software development and IT operations, our coding bootcamp focuses on building websites and mobile phone apps. The application process takes 2-3 weeks and we would advise you to apply sooner rather than later to give yourself plenty of time to work through the preparation materials. Applicants living in England can apply for DfE funding to cover the entire cost of the course. Get in touch to find out if you qualify."
+
+test_input3 = "As a Data Analyst graduate you will be able to problem solve and effectively communicate like an analyst. This course teaches you to use industry-standard tools to make ethical, data-driven decisions. Experience hands-on training to master SQL, Excel, Tableau, PowerBI, and Python – tools listed in virtually every data analytics job posting across industries."
+
+test_input4 = "The course is designed for everyone, whether a complete novice, a computer science graduate wanting practical experience, or an entrepreneur sick of looking for a technical co-founder. Students learn an incredible amount, including: Ruby on Rails; HTML5 and CSS3; time management; communication skills; customer service; Agile and Lean Development; JavaScript, jQuery and NodeJS; along with Git and Heroku, and software design best practices. Students learn through first hand experience, community-driven classrooms, pairing, and project-based work."
+
+test_input5 = "On our Data Science course, you will learn the fundamentals of this discipline, including Python, how to make dashboards using PowerBI, database management and PSQL. We do not cover JavaScript, Front End nor how to make a website using HTML."
+
 
 def test_returns_correct_typing():
-    test_input = "As a Software Engineer graduate you will be ready to start a career in a variety of coding roles. Throughout this bootcamp you will learn to: create front-end web application with modern JavaScript frameworks such as Angular or React, develop full-stack applications with in-demand technologies such as Ruby on Rails, Python with Django, and Express with Node.js, and integrate third-party application programming interfaces (APIs) in an application."
-
-    test_input2 = "Become a software engineer in 13 weeks at our coding bootcamps in Manchester, Leeds, Newcastle, Birmingham and remotely. Unlike our Data Engineering bootcamp where you focus specifically on the 'back-end' of software, or our DevOps Engineering bootcamp that deals specifically with software development and IT operations, our coding bootcamp focuses on building websites and mobile phone apps. The application process takes 2-3 weeks and we would advise you to apply sooner rather than later to give yourself plenty of time to work through the preparation materials. Applicants living in England can apply for DfE funding to cover the entire cost of the course. Get in touch to find out if you qualify."
-
-    test_input3 =  "As a Data Analyst graduate you will be able to problem solve and effectively communicate like an analyst. This course teaches you to use industry-standard tools to make ethical, data-driven decisions. Experience hands-on training to master SQL, Excel, Tableau, PowerBI, and Python – tools listed in virtually every data analytics job posting across industries."
-
-
-    result = extract_skills(test_input3)
-
-    print('RESULT', result)
+    result = extract_skills(test_input5)
 
     for skill in result:
         assert type(skill) is str
+
+
+def test_returns_expected_list_of_skills():
+    result = extract_skills(test_input5)
+
+    expected = ['Python', 'PowerBI', 'Database Management', 'PSQL']
+
+    assert result == expected
--- processors/processor_course_report/extract_skills.py
+++ processors/processor_course_report/extract_skills.py
@@ -8,12 +8,29 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 def extract_skills(description):
+    """
+    Runs an openAI API query to return a list of digital skills from a given course description
 
-    technologies = 'JavaScript, Front-End'
+    :param description: a string detailing what a bootcamp course offers
+    :type description: string
+    :return: list of digital skills included in the course description
+    :rtype: list
+    """
+
+    examples = 'JavaScript, Front-End, HTML, CSS, Node.js'
+
+    prompt1 = f'Extract a single, comma separated list of technologies, programming languages, and frameworks from the following description where the course purports to teach this :\n\n """{description}""" \n which are similar to or equal to these examples: \n """{examples}""" '
+
+    # Alternative prompt following prompt writing guidelines, though gave slightly worse results
+    # prompt2 = f'''Extract a single, comma separated list of technologies, programming languages, and frameworks from the description below which are similar to or equal to these examples: {examples}
+
+    # ###
+    # Description: {description}
+    # '''
 
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=f'Extract a single, comma separated list of technologies, programming languages, and frameworks from the following description where the course purports to teach this :\n\n """{description}""" \n which are similar to or equal to these examples: \n """{technologies}""" ',
+        prompt=prompt1,
         max_tokens=100,
         top_p=0.1,
         temperature=1,
@@ -22,7 +39,5 @@ def extract_skills(description):
     )
 
     output = response.choices[0].text.strip()
-
-    print('OUTPUT', output)
 
     return output.split(', ')
