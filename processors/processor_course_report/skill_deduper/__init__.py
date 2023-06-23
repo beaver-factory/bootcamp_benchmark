5a3f75d9237f2a0569873ed110d2eead70199c5d
Matthew Scarth-Saunders <101186068+MattScarthSaunders@users.noreply.github.com>
2023-06-23 12:54:56 +0100

    feat: skill deduper function

--- processors/processor_course_report/skill_deduper/__init__.py
+++ processors/processor_course_report/skill_deduper/__init__.py
@@ -1,89 +1,43 @@
 import pandas as pd
 from pandas import DataFrame
-import os
-import openai
 from typing import List
 from dotenv import load_dotenv
+from azure.functions import InputStream, Out
+from pathlib import Path
+import os
+import openai
 import json
 
 load_dotenv()
 
 
-# def skill_deduper() -> DataFrame:
-#     """Removes duplicates of skills e.g. ["React", "react.js"] becomes ["react"]
+def check_edge_case_dict(df: DataFrame, inBlob: InputStream, outBlob: Out[bytes]) -> DataFrame:
+    # handle input
+    input_json = inBlob.read().decode('utf-8')
 
-#     :param df: DataFrame containing skills
-#     :type df: DataFrame
-#     :return: DataFrame containing skill
-#     :rtype: DataFrame
-#     """
-#     openai.api_key = os.environ["OPENAI_API_KEY"]
+    if input_json == '':
+        raise Exception('inBlob is empty, check skills_dict.json exists')
 
-#     # skills_list = df["course_skills"][0]
-#     # prepared_skills_list = prep_prompt_input(skills_list)
+    skills_dict = json.loads(input_json)
+    new_skills_dict = skills_dict.copy()
 
-#     response = openai.Completion.create(
-#         model="text-davinci-003",
-#         prompt=f"""We are using python to create a list of software development technologies and programming skills, which currently includes lots of similar values:['agile', 'angular', 'angularjs', 'data engineering', 'devops', 'express', 'express.js', 'javascript', 'js', 'mysql', 'networking', 'node', 'node.js', 'nodejs', 'nodered', 'postgresql', 'postgresql', 'problem solving', 'psql', 'react', 'react.js', 'reactjs', 'sql', 'ts', 'typescript'].
-#         Please perform the following steps in this order:
-#         1. Remove any values from the list that are the same as other values in the list, but that also include an additional suffix.
-#         2. Remove any values that are abbreviations of other values.
-#         3. Remove any values that are acronyms of other values.
-#         4. Return one list of unique values, and one list of the extracted related values.
-#         Please return only the lists and no other words or phrases.\n \n""",
-#         temperature=1,
-#         max_tokens=1000,
-#         top_p=0.1,
-#         frequency_penalty=0.8,
-#         presence_penalty=0.0
-#     )
+    new_df = df.copy(deep=True)
+    course_skills = new_df["course_skills"][0]
 
-#     print('response 1:', response["choices"][0]['text'])
+    # dict checker
+    for i in range(len(course_skills)):
+        skill = course_skills[i]
+        new_df["course_skills"][0][i] = new_df["course_skills"][0][i].lower()
 
-#     # response_string = response["choices"][0]["text"]
-#     # actual = response_string[8:]
-#     # print(actual)
-#     # edgy_skills = check_edge_case_dict(actual)
+        try:
+            key = next(key for key, value in skills_dict.items() if skill in value)
+            new_df["course_skills"][0][i] = key
+        except:
+            new_skills_dict[skill] = [skill]
 
-#     # return pd.DataFrame([{"skills": edgy_skills}])
+    # handle output
+    if set(new_skills_dict.keys()) - set(skills_dict.keys()):
+        output = json.dumps(new_skills_dict)
+        outBlob.set(output.encode('utf-8'))
 
-
-def prep_prompt_input(skills_list: List[str]) -> List[str]:
-    """Performs essential preprocessing of skills list before handing to OpenAI
-
-    :param skills_list: List of skills
-    :type df: List[str]
-    :raises Exception: skill list error
-    :return: list of skill strings
-    :rtype: List[str]
-    """
-
-    if len(skills_list) == 0:
-        raise Exception("Cannot prep prompt, list of skills is empty")
-
-    lower_skills = [x.lower() for x in skills_list]
-
-    prepared_skills_list = list(dict.fromkeys(lower_skills))
-
-    sorted_list = sorted(prepared_skills_list)
-    print(sorted_list)
-
-
-def check_edge_case_dict(skills_list: List[str]) -> List[str]:
-    # INPUT course_report df and output course_report df
-
-    skills_to_dedupe = {
-        "html5": "html",
-    }
-
-    for skill in skills_list:
-        if skill in skills_to_dedupe and skills_to_dedupe[skill] in skills_list:
-            skills_list.remove(skill)
-        elif skill in skills_to_dedupe:
-            skills_list[skills_list.index(skill)] = skills_to_dedupe[skill]
-
-    return skills_list
-
-
-# prep_prompt_input(["react", "react.JS", "react.js", "React", "React.JS", "ReactJS", "posgresql", "SQL", "postgreSQL", "PSQL", "psql", "angular", "angularJS", "angularjs", "Express", "express.js", "Agile", "problem solving", "data engineering", "mysql", "devops", "Networking", "Node", "node.js", "nodeRED", "NodeJS", "js", "JavaScript", "ts", "TypeScript"])
-skill_deduper()
+    return new_df
--- processors/processor_course_report/skill_deduper/__tests__/test_skill_deduper.py
+++ processors/processor_course_report/skill_deduper/__tests__/test_skill_deduper.py
@@ -1,96 +1,57 @@
-from processor_course_report.skill_deduper import skill_deduper, prep_prompt_input, check_edge_case_dict
+from processor_course_report.skill_deduper import check_edge_case_dict
+from processor_utils import generate_inputstream
 import pandas as pd
 from dotenv import load_dotenv
 import pytest
 from unittest.mock import patch
+from io import BytesIO
 
 load_dotenv()
 
-
-@patch("openai.Completion.create", return_value={"choices": [{"text": "html"}]})
-def test_outputs_df(openai_mock):
-    test_empty_dataframe = pd.DataFrame([{"skills": ["html"]}])
-
-    result = skill_deduper(test_empty_dataframe)
-
-    assert openai_mock.call_count == 1
-    assert isinstance(result, pd.DataFrame)
-    assert result["skills"][0] == ["html"]
+dirpath = 'processor_course_report/skill_deduper/skills_dict.json'
 
 
-@pytest.mark.skip(reason='avoid OpenAI API call')
-def test_removes_similar_values():
-    test_react_dataframe = pd.DataFrame([{"skills": ['react', 'React', 'react.js', 'React.js', 'react.JS', 'React.JS']}])
+@patch('azure.functions.Out')
+def test_check_edge_case_dict_does_nothig_if_all_valid(outblob):
+    new_inputstream = generate_inputstream(dirpath)
+    df = pd.DataFrame([{"course_skills": ['html', 'react', 'express']}])
 
-    result = skill_deduper(test_react_dataframe)
+    result = check_edge_case_dict(df, new_inputstream, outblob)
+    skill_list = result["course_skills"][0]
 
-    result_arr = result['skills'][0]
-
-    assert result_arr == ['react']
-
-
-def test_prep_prompt_input_sorts_alphabetically():
-    skills = ["react", "html", "css"]
-    df = pd.DataFrame([{"skills": skills}])
-
-    result = prep_prompt_input(df)
-
-    expected = ["css", "html", "react"]
-    assert result == expected
+    assert skill_list[0] == 'html'
+    assert outblob.set.call_count == 0
 
 
-def test_prep_prompt_input_lowercases_everything():
-    skills = ["Javascript", "postgreSQL", "react.JS"]
-    df = pd.DataFrame([{"skills": skills}])
+@patch('azure.functions.Out')
+def test_check_edge_case_dict_replaces_values(outblob):
+    new_inputstream = generate_inputstream(dirpath)
+    df = pd.DataFrame([{"course_skills": ['html5']}])
 
-    result = prep_prompt_input(df)
+    result = check_edge_case_dict(df, new_inputstream, outblob)
+    skill_list = result["course_skills"][0]
 
-    expected = ["javascript", "postgresql", "react.js"]
-    assert result == expected
-
-
-def test_prep_prompt_input_returns_only_unique_values():
-    skills = ["react", "react.JS", "react.js"]
-    df = pd.DataFrame([{"skills": skills}])
-
-    result = prep_prompt_input(df)
-
-    expected = ["react", "react.js"]
-    assert result == expected
+    assert skill_list[0] == 'html'
 
 
-def test_prep_prompt_input_raises_exception_when_passed_empty_skills_list():
-    skills = []
-    df = pd.DataFrame([{"skills": skills}])
+@patch('azure.functions.Out')
+def test_check_edge_case_dict_outputs_new_blob(outblob):
+    new_inputstream = generate_inputstream(dirpath)
+    df = pd.DataFrame([{"course_skills": ['test1', 'test2']}])
 
-    with pytest.raises(Exception) as error:
-        prep_prompt_input(df)
+    result = check_edge_case_dict(df, new_inputstream, outblob)
+    skill_list = result["course_skills"][0]
 
-    assert str(error.value) == "Cannot prep prompt, list of skills is empty"
-
-
-def test_check_edge_case_dict_removes_edge_case():
-    skills = ["react", "html", "html5"]
-
-    result = check_edge_case_dict(skills)
-
-    expected = ["react", "html"]
-    assert result == expected
+    assert outblob.set.call_count == 1
+    assert skill_list[0] == 'test1'
 
 
-def test_check_edge_case_dict_returns_empty_string_when_passed_empty_string():
-    skills = [""]
+@patch('azure.functions.Out')
+def test_check_edge_case_dict_outputs_lowercase_df(outblob):
+    new_inputstream = generate_inputstream(dirpath)
+    df = pd.DataFrame([{"course_skills": ['HTML']}])
 
-    result = check_edge_case_dict(skills)
+    result = check_edge_case_dict(df, new_inputstream, outblob)
+    skill_list = result["course_skills"][0]
 
-    expected = [""]
-    assert result == expected
-
-
-def test_check_edge_case_dict_updates_single_edge_case_skill():
-    skills = ["react", "html5"]
-
-    result = check_edge_case_dict(skills)
-
-    expected = ["react", "html"]
-    assert result == expected
+    assert skill_list[0] == 'html'
