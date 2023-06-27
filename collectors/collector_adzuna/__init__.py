dbba9bc09e2d5055798d0a12335c90fc63ce12f5
Alex Cupit <alexcupit@outlook.com>
2023-06-27 15:55:50 +0100

    refactor: remove latest_skills.csv from binding and init

--- collectors/collector_adzuna/__init__.py
+++ collectors/collector_adzuna/__init__.py
@@ -2,18 +2,16 @@ from .app import collector_adzuna
 from azure.functions import TimerRequest, InputStream, Out
 
 
-def main(mytimer: TimerRequest, inLatestSkills: InputStream, inSkillsDict: InputStream, outAdzunaJobCounts: Out[bytes]) -> None:
+def main(mytimer: TimerRequest, inSkillsDict: InputStream, outAdzunaJobCounts: Out[bytes]) -> None:
     """Main function of collector_adzuna
 
     :param mytimer: Azure timer trigger
     :type mytimer: TimerRequest
-    :param inLatestSkills: Azure input blob
-    :type inLatestSkills: InputStream
     :param inSkillsDict: Azure input blob
     :type inSkillsDict: InputStream
     :param outAdzunaJobCounts: Azure output blob
     :type outAdzunaJobCounts: Out[bytes]
     """
-    api_data = collector_adzuna(inLatestSkills, inSkillsDict)
+    api_data = collector_adzuna(inSkillsDict)
 
     outAdzunaJobCounts.set(api_data.encode('utf-8'))
