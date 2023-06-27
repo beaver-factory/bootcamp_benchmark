5eaae3b0b061f532e716da8e1831dab21ce03c9f
Alex Cupit <alexcupit@outlook.com>
2023-06-27 15:53:37 +0100

    test: remove testing of csv input streams and add in more local json creation for different scenarios

--- collectors/collector_adzuna/__tests__/test_app.py
+++ collectors/collector_adzuna/__tests__/test_app.py
@@ -5,22 +5,8 @@ import requests_mock
 import pytest
 import os
 import shutil
-import csv
 
-dirpath = 'collector_adzuna/__tests__/csv'
-
-
-@pytest.fixture(scope="session", autouse=True)
-def create_csv():
-
-    if os.path.exists(dirpath):
-        shutil.rmtree(dirpath)
-
-    generate_csv()
-
-    yield
-
-    shutil.rmtree(dirpath)
+dirpath = 'collector_adzuna/__tests__/json'
 
 
 test_dict = {'Express': ['express', 'expressjs', 'express.js'], 'CSS': ['css', 'css3.0'], 'HTML': ['html', 'html5'], 'React': ['react', 'react.js', 'reactjs'], 'Java': ['java'], 'Asana': ['asana'], 'Data Engineering': ['data engineering'], 'GCP': ['gcp', 'google cloud', 'google cloud platform'], 'C#': ['c#']}
@@ -29,34 +15,19 @@ test_dict = {'Express': ['express', 'expressjs', 'express.js'], 'CSS': ['css', '
 @pytest.fixture(scope="session", autouse=True)
 def create_skills_json():
 
-    path = 'collector_adzuna/__tests__/skills_dict.json'
+    if os.path.exists(dirpath):
+        shutil.rmtree(dirpath)
 
-    if os.path.isfile(path):
-        os.remove(path)
-
-    with open(f'{path}', 'w') as file:
-        file.write(json.dumps(test_dict))
+    generate_test_jsons()
 
     yield
 
-    os.remove(path)
+    shutil.rmtree(dirpath)
 
 
-def generate_inputstream(path):
-    """converts a local csv file and returns a mocked blob input stream containing that data"""
+def generate_skills_dict_input_stream(path):
+    """converts a local json file and returns a mocked blob input stream containing that data"""
     with open(path, 'rb') as file:
-        test_csv_data = file.read()
-
-    mock_inputstream = Mock()
-    mock_inputstream.read.return_value = test_csv_data
-
-    with patch('azure.functions.InputStream', return_value=mock_inputstream):
-        return mock_inputstream
-
-
-def generate_skills_dict_input_stream():
-    """converts a local csv file and returns a mocked blob input stream containing that data"""
-    with open('collector_adzuna/__tests__/skills_dict.json', 'rb') as file:
         test_skills_dict_json = file.read()
 
     mock_inputstream = Mock()
@@ -66,21 +37,41 @@ def generate_skills_dict_input_stream():
         return mock_inputstream
 
 
+def generate_test_jsons():
+
+    os.mkdir(dirpath)
+
+    with open(dirpath + '/single_skills_dict.json', 'w') as file:
+        file.write(json.dumps({'Java': ['java']}))
+
+    with open(dirpath + '/no_synonyms_skills_dict.json', 'w') as file:
+        file.write(json.dumps({'Java': ['java'], 'Asana': ['asana']}))
+
+    with open(dirpath + '/empty_skills_dict.json', 'w') as file:
+        file.write(json.dumps({}))
+
+    with open(dirpath + '/phrases_in_skills_dict.json', 'w') as file:
+        file.write(json.dumps({'Data Engineering': ['data engineering']}))
+
+    with open(dirpath + '/single_word_synonyms_in_skills_dict.json', 'w') as file:
+        file.write(json.dumps({'React': ['react', 'react.js', 'reactjs']}))
+
+    with open(dirpath + '/multi_word_synonyms_in_skills_dict.json', 'w') as file:
+        file.write(json.dumps({'GCP': ["gcp", "google cloud", "google cloud platform"]}))
+
+
 @patch('collector_adzuna.app.get_secret_value', return_value='test')
-def test_one_request_is_made_to_API_with_correctly_formatted_result(mock, create_csv):
+def test_one_request_is_made_to_API_with_correctly_formatted_result(mock):
     with requests_mock.Mocker() as m:
-        m.get('http://api.adzuna.com/v1/api/jobs/gb/search/1?title_only=junior&location0=UK&category=it-jobs&content-type=application/json',
+        m.get('http://api.adzuna.com/v1/api/jobs/gb/search/1?title_only=junior&location0=UK&category=it-jobs&content-type=application/json&what=java',
               json={"count": 227})
 
-        test_csv_input_stream = generate_inputstream(
-            "./collector_adzuna/__tests__/csv/test_one_skill.csv")
+        test_json_input_stream = generate_skills_dict_input_stream(dirpath + '/single_skills_dict.json')
 
-        test_json_input_stream = generate_skills_dict_input_stream()
-
-        result = collector_adzuna(test_csv_input_stream, test_json_input_stream)
+        result = collector_adzuna(test_json_input_stream)
 
         assert m.call_count == 1
-        assert result == json.dumps({"HTML": 227})
+        assert result == json.dumps({"Java": 227})
 
 
 @patch('collector_adzuna.app.get_secret_value', return_value='test')
@@ -91,32 +82,28 @@ def test_multiple_requests_made_to_API_with_correctly_formatted_result(mock):
         m.get('http://api.adzuna.com/v1/api/jobs/gb/search/1?what=asana&title_only=junior&location0=UK&category=it-jobs&content-type=application/json',
               json={"count": 4608})
 
-        test_input_stream = generate_inputstream(
-            "./collector_adzuna/__tests__/csv/test_skills.csv")
+        test_json_input_stream = generate_skills_dict_input_stream(dirpath + '/no_synonyms_skills_dict.json')
 
-        test_json_input_stream = generate_skills_dict_input_stream()
-
-        result = collector_adzuna(test_input_stream, test_json_input_stream)
+        result = collector_adzuna(test_json_input_stream)
 
         assert m.call_count == 2
-        assert result == json.dumps({"java": 227, "asana": 4608})
+        assert result == json.dumps({"Java": 227, "Asana": 4608})
 
 
-def test_error_raised_when_skills_csv_is_empty():
-    test_input_stream = generate_inputstream(
-        "./collector_adzuna/__tests__/csv/test_skills_empty.csv")
+def test_error_raised_when_skills_dict_is_empty():
 
-    test_json_input_stream = generate_skills_dict_input_stream()
+    test_json_input_stream = generate_skills_dict_input_stream(dirpath + '/empty_skills_dict.json')
 
     with pytest.raises(Exception) as error:
-        collector_adzuna(test_input_stream, test_json_input_stream)
+        collector_adzuna(test_json_input_stream)
 
-    assert str(error.value) == 'List of skills is empty, CSV may be empty'
+    assert str(error.value) == 'inSkillsDict is empty, check skills_dict.json exists'
 
 
 def test_create_keyword_query_returns_same_keyword_when_no_processing_needed():
-    keyword = "java"
-    result = create_keyword_query(keyword, test_dict)
+    keyword = "Java"
+    synonyms = test_dict[keyword]
+    result = create_keyword_query(synonyms)
     expected = "what=java"
 
     assert result == expected
@@ -124,31 +111,26 @@ def test_create_keyword_query_returns_same_keyword_when_no_processing_needed():
 
 def test_create_keyword_query_returns_variants_of_skill_when_matches_key_in_skills_dict():
     keyword = "React"
-    result = create_keyword_query(keyword, test_dict)
-    expected = "what_or=react%20react.js%20reactjs"
-
-    assert result == expected
-
-
-def test_create_keyword_query_returns_variants_of_skill_when_matches_value_in_skills_dict():
-    keyword = "React.js"
-    result = create_keyword_query(keyword, test_dict)
+    synonyms = test_dict[keyword]
+    result = create_keyword_query(synonyms)
     expected = "what_or=react%20react.js%20reactjs"
 
     assert result == expected
 
 
 def test_create_keyword_query_returns_a_what_phrase_when_multi_word_passed_in():
-    keyword = 'data engineering'
-    result = create_keyword_query(keyword, test_dict)
+    keyword = 'Data Engineering'
+    synonyms = test_dict[keyword]
+    result = create_keyword_query(synonyms)
     expected = 'what_phrase=data%20engineering'
 
     assert result == expected
 
 
 def test_create_keyword_query_returns_what_and_key_when_synonynms_contains_spaces():
-    keyword = 'google cloud platform'
-    result = create_keyword_query(keyword, test_dict)
+    keyword = 'GCP'
+    synonyms = test_dict[keyword]
+    result = create_keyword_query(synonyms)
     expected = 'what=gcp'
 
     assert result == expected
@@ -156,52 +138,50 @@ def test_create_keyword_query_returns_what_and_key_when_synonynms_contains_space
 
 def test_create_keyword_query_returns_encoded_chars():
     keyword = 'C#'
-    result = create_keyword_query(keyword, test_dict)
+    synonyms = test_dict[keyword]
+    result = create_keyword_query(synonyms)
     expected = 'what=c%23'
 
     assert result == expected
 
 
 @patch('collector_adzuna.app.get_secret_value', return_value='test')
-def test_api_called_with_expected_url_when_synonyms_present(mock):
+def test_api_called_with_expected_url_when_phrases_present(mock):
     with requests_mock.Mocker() as m:
-        m.get('http://api.adzuna.com/v1/api/jobs/gb/search/1?what_or=react%20react.js%20reactjs&title_only=junior&location0=UK&category=it-jobs&content-type=application/json',
-              json={"count": 227})
         m.get('http://api.adzuna.com/v1/api/jobs/gb/search/1?what_phrase=data%20engineering&title_only=junior&location0=UK&category=it-jobs&content-type=application/json',
               json={"count": 4608})
 
-        test_input_stream = generate_inputstream(
-            "./collector_adzuna/__tests__/csv/test_skills_with_synonyms.csv")
+        test_json_input_stream = generate_skills_dict_input_stream(dirpath + '/phrases_in_skills_dict.json')
 
-        test_json_input_stream = generate_skills_dict_input_stream()
+        result = collector_adzuna(test_json_input_stream)
 
-        result = collector_adzuna(test_input_stream, test_json_input_stream)
-
-        assert m.call_count == 2
-        assert result == json.dumps({"react": 227, "data engineering": 4608})
+        assert m.call_count == 1
+        assert result == json.dumps({"Data Engineering": 4608})
 
 
-def generate_csv():
-    """Creates a series of CSVs containing data needed for tests"""
+@patch('collector_adzuna.app.get_secret_value', return_value='test')
+def test_api_called_with_expected_url_when_single_word_synonyms_present(mock):
+    with requests_mock.Mocker() as m:
+        m.get('http://api.adzuna.com/v1/api/jobs/gb/search/1?what_or=react%20react.js%20reactjs&title_only=junior&location0=UK&category=it-jobs&content-type=application/json',
+              json={"count": 227})
 
-    os.mkdir(f'{dirpath}')
+        test_json_input_stream = generate_skills_dict_input_stream(dirpath + '/single_word_synonyms_in_skills_dict.json')
 
-    with open(f'{dirpath}/test_one_skill.csv', 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter=',', lineterminator='\n')
-        csvwriter.writerow(['', 'course_skills'])
-        csvwriter.writerow(['0', 'HTML'])
+        result = collector_adzuna(test_json_input_stream)
 
-    with open(f'{dirpath}/test_skills_empty.csv', 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter=',', lineterminator='\n')
+        assert m.call_count == 1
+        assert result == json.dumps({"React": 227})
 
-    with open(f'{dirpath}/test_skills.csv', 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter=',', lineterminator='\n')
-        csvwriter.writerow(['', 'course_skills'])
-        csvwriter.writerow(['0', 'java'])
-        csvwriter.writerow(['1', 'asana'])
 
-    with open(f'{dirpath}/test_skills_with_synonyms.csv', 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter=',', lineterminator='\n')
-        csvwriter.writerow(['', 'course_skills'])
-        csvwriter.writerow(['0', 'react'])
-        csvwriter.writerow(['1', 'data engineering'])
+@patch('collector_adzuna.app.get_secret_value', return_value='test')
+def test_api_called_with_expected_url_when_multi_word_synonyms_present(mock):
+    with requests_mock.Mocker() as m:
+        m.get('http://api.adzuna.com/v1/api/jobs/gb/search/1?what=gcp&title_only=junior&location0=UK&category=it-jobs&content-type=application/json',
+              json={"count": 100})
+
+        test_json_input_stream = generate_skills_dict_input_stream(dirpath + '/multi_word_synonyms_in_skills_dict.json')
+
+        result = collector_adzuna(test_json_input_stream)
+
+        assert m.call_count == 1
+        assert result == json.dumps({"GCP": 100})
